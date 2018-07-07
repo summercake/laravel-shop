65fdafd4dffb5d996f5bda430bc0adc0f884835e
summercake@qq.com <summercake@qq.com>
2018-07-07 02:25:41 -0500

    basic layout done

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 let mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +9,6 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+	.sass('resources/assets/sass/app.scss', 'public/css')
+	.version();
