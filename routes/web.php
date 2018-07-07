87e67535dd5816c097fa3ad4a38d4ddeabb04045
summercake@qq.com <summercake@qq.com>
2018-07-07 03:20:11 -0500

    email verification done

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     // 开始
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+
     });
     // 结束
 });
