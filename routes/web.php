1b8175ee59f6f80b1a5812c704119d1aed1946f3
summercake@qq.com <summercake@qq.com>
2018-07-07 20:55:50 -0500

    favorite products page done

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 */
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function (){
@@ -30,6 +29,8 @@ Route::group(['middleware' => 'auth'], function (){
             'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
     // 结束
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
