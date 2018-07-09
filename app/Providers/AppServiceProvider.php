cfe157dd111adabf53d1b7f8c0352c28e7975bea
summercake@qq.com <summercake@qq.com>
2018-07-09 03:29:50 -0500

    order payment done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Logger;
 use Yansongda\Pay\Pay;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,11 +25,14 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // 往服务容器中注入一个名为 alipay 的单例对象
-        $this->app->singleton('alipay', function () {
+        $this->app->singleton('alipay', function (){
             $config = config('pay.alipay');
+            //$config['notify_url'] = route('payment.alipay.notify');
+            $config['notify_url'] = 'http://requestbin.leo108.com/13nt0li1';
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
-                $config['mode']         = 'dev';
+                $config['mode'] = 'dev';
                 $config['log']['level'] = Logger::DEBUG;
             } else {
                 $config['log']['level'] = Logger::WARNING;
@@ -38,8 +40,7 @@ class AppServiceProvider extends ServiceProvider
             // 调用 Yansongda\Pay 来创建一个支付宝支付对象
             return Pay::alipay($config);
         });
-
-        $this->app->singleton('wechat_pay', function () {
+        $this->app->singleton('wechat_pay', function (){
             $config = config('pay.wechat');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
