16a070e2447d866a619bc86630d78e444743a5fa
summercake@qq.com <summercake@qq.com>
2018-07-10 07:44:34 -0500

    alipay refund done

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -49,33 +49,37 @@
             <!-- 订单发货开始 -->
             <!-- 如果订单未发货，展示发货表单 -->
             @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
-                <tr>
-                    <td colspan="4">
-                        <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
-                            <!-- 别忘了 csrf token 字段 -->
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
-                                <label for="express_company" class="control-label">物流公司</label>
-                                <input type="text" id="express_company" name="express_company" value="" class="form-control" placeholder="输入物流公司">
-                                @if($errors->has('express_company'))
-                                    @foreach($errors->get('express_company') as $msg)
-                                        <span class="help-block">{{ $msg }}</span>
-                                    @endforeach
-                                @endif
-                            </div>
-                            <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
-                                <label for="express_no" class="control-label">物流单号</label>
-                                <input type="text" id="express_no" name="express_no" value="" class="form-control" placeholder="输入物流单号">
-                                @if($errors->has('express_no'))
-                                    @foreach($errors->get('express_no') as $msg)
-                                        <span class="help-block">{{ $msg }}</span>
-                                    @endforeach
-                                @endif
-                            </div>
-                            <button type="submit" class="btn btn-success" id="ship-btn">发货</button>
-                        </form>
-                    </td>
-                </tr>
+                <!-- 加上这个判断条件 -->
+                @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS)
+                    <tr>
+                        <td colspan="4">
+                            <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
+                                <!-- 别忘了 csrf token 字段 -->
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
+                                    <label for="express_company" class="control-label">物流公司</label>
+                                    <input type="text" id="express_company" name="express_company" value="" class="form-control" placeholder="输入物流公司">
+                                    @if($errors->has('express_company'))
+                                        @foreach($errors->get('express_company') as $msg)
+                                            <span class="help-block">{{ $msg }}</span>
+                                        @endforeach
+                                    @endif
+                                </div>
+                                <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
+                                    <label for="express_no" class="control-label">物流单号</label>
+                                    <input type="text" id="express_no" name="express_no" value="" class="form-control" placeholder="输入物流单号">
+                                    @if($errors->has('express_no'))
+                                        @foreach($errors->get('express_no') as $msg)
+                                            <span class="help-block">{{ $msg }}</span>
+                                        @endforeach
+                                    @endif
+                                </div>
+                                <button type="submit" class="btn btn-success" id="ship-btn">发货</button>
+                            </form>
+                        </td>
+                    </tr>
+            <!-- 在 上一个 if 的 else 前放上 endif -->
+                @endif
             @else
                 <!-- 否则展示物流公司和物流单号 -->
                 <tr>
@@ -115,13 +119,13 @@
 				closeOnConfirm: false,
 				confirmButtonText: "确认",
 				cancelButtonText: "取消",
-			}, function(inputValue){
+			}, function(inputValue) {
 				// 用户点击了取消，inputValue 为 false
 				// === 是为了区分用户点击取消还是没有输入
-				if (inputValue === false) {
+				if(inputValue === false) {
 					return;
 				}
-				if (!inputValue) {
+				if(!inputValue) {
 					swal('理由不能为空', '', 'error')
 					return;
 				}
@@ -137,12 +141,45 @@
 						_token: LA.token,
 					}),
 					contentType: 'application/json',  // 请求的数据格式为 JSON
-					success: function (data) {  // 返回成功时会调用这个函数
+					success: function(data) {  // 返回成功时会调用这个函数
 						swal({
 							title: '操作成功',
 							type: 'success'
 						}, function() {
 							// 用户点击 swal 上的 按钮时刷新页面
+							location.reload();
+						});
+					}
+				});
+			});
+		});
+		// 同意按钮的点击事件
+		$('#btn-refund-agree').click(function() {
+			swal({
+				title: '确认要将款项退还给用户？',
+				type: 'warning',
+				showCancelButton: true,
+				closeOnConfirm: false,
+				confirmButtonText: "确认",
+				cancelButtonText: "取消",
+			}, function(ret) {
+				// 用户点击取消，不做任何操作
+				if(!ret) {
+					return;
+				}
+				$.ajax({
+					url: '{{ route('admin.orders.handle_refund', [$order->id]) }}',
+					type: 'POST',
+					data: JSON.stringify({
+						agree: true, // 代表同意退款
+						_token: LA.token,
+					}),
+					contentType: 'application/json',
+					success: function(data) {
+						swal({
+							title: '操作成功',
+							type: 'success'
+						}, function() {
 							location.reload();
 						});
 					}
