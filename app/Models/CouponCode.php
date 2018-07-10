b7ca9cf93a043485f387ca186476b460859b1a1e
summercake@qq.com <summercake@qq.com>
2018-07-10 08:40:28 -0500

    coupon code model done

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
+use App\Exceptions\CouponCodeUnavailableException;
 
 class CouponCode extends Model
 {
@@ -81,6 +83,44 @@ class CouponCode extends Model
             return $this->newQuery()->where('id', $this->id)->where('used', '<', $this->total)->increment('used');
         } else {
             return $this->decrement('used');
+        }
+    }
+
+    public function checkAvailable($orderAmount = null)
+    {
+        if (!$this->enabled) {
+            throw new CouponCodeUnavailableException('优惠券不存在');
+        }
+
+        if ($this->total - $this->used <= 0) {
+            throw new CouponCodeUnavailableException('该优惠券已被兑完');
+        }
+
+        if ($this->not_before && $this->not_before->gt(Carbon::now())) {
+            throw new CouponCodeUnavailableException('该优惠券现在还不能使用');
+        }
+
+        if ($this->not_after && $this->not_after->lt(Carbon::now())) {
+            throw new CouponCodeUnavailableException('该优惠券已过期');
+        }
+
+        if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
+            throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
+        }
+        $used = Order::where('user_id', $user->id)
+                     ->where('coupon_code_id', $this->id)
+                     ->where(function($query) {
+                         $query->where(function($query) {
+                             $query->whereNull('paid_at')
+                                   ->where('closed', false);
+                         })->orWhere(function($query) {
+                             $query->whereNotNull('paid_at')
+                                   ->where('refund_status', Order::REFUND_STATUS_PENDING);
+                         });
+                     })
+                     ->exists();
+        if ($used) {
+            throw new CouponCodeUnavailableException('你已经使用过这张优惠券了');
         }
     }
 }
